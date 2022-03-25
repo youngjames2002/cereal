fabc1d5712caf98fe6d872e7cfa715f7bb5453aa
youngjames2002 <51745296+youngjames2002@users.noreply.github.com>
2022-03-25 18:03:21 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+function main() {
+
+    startAlert();
+    gameStart();
+
+}
+
+function startAlert() {
+
+    let name = document.getElementById('name');
+    let nameVal = name.value;
+
+    alert("Hey " + nameVal + "! You have 5 minutes, MAKE CEREAL DUMMY!!");
+
+}
+
+function gameStart(){
+    startTimer();
+}
+
+function startTimer(){
+
+}
