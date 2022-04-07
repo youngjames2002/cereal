205dd526dc773a59a0cbd864a93c9e32c0b01c56
toastservant <31703778+toastservant@users.noreply.github.com>
2022-04-08 00:26:16 +0100

    formatting

--- script.js
+++ script.js
@@ -232,7 +232,7 @@ const textNodes = [
     id: 12,
     text: "That's all for now bozo",
     options: [
-     
+
     ]
   },
 ]
@@ -241,8 +241,8 @@ startGame()
 
 function main() {
 
-    startAlert();
-    gameStart();
+  startAlert();
+  gameStart();
 
 }
 // stats variables declaration
@@ -251,211 +251,211 @@ const places = [];
 
 function startAlert() {
 
-    let name = document.getElementById('name');
-    let nameVal = name.value;
+  let name = document.getElementById('name');
+  let nameVal = name.value;
 
-    if (nameVal == "") {
-        alert('Enter your name please!');
-    } else {
+  if (nameVal == "") {
+    alert('Enter your name please!');
+  } else {
 
-        alert("Hey " + nameVal + "! You have 5 minutes, MAKE CEREAL DUMMY!!");
+    alert("Hey " + nameVal + "! You have 5 minutes, MAKE CEREAL DUMMY!!");
 
-        let timerLink = document.getElementById('link');
-        timerLink.style.visibility = 'visible';
-    }
+    let timerLink = document.getElementById('link');
+    timerLink.style.visibility = 'visible';
+  }
 }
 
 function gameStart() {
-    startTimer();
+  startTimer();
 
-    let message = document.getElementById('message');
-    message.innerHTML = "Good Morning! You have just awoken on another beautiful day and you feel your stomach rumble! Go make yourself some cereal to start the day off right!";
+  let message = document.getElementById('message');
+  message.innerHTML = "Good Morning! You have just awoken on another beautiful day and you feel your stomach rumble! Go make yourself some cereal to start the day off right!";
 }
 
 var myTimer;
 var duration = 3000;
 
 function startTimer() {
-    myTimer = setInterval('displayTimer()', 1000);
-    displayTimer();
+  myTimer = setInterval('displayTimer()', 1000);
+  displayTimer();
 }
 
 function displayTimer() {
-    duration--;
-    if (duration >= 0) {
-        document.getElementById('timer').innerHTML = duration;
-    }
+  duration--;
+  if (duration >= 0) {
+    document.getElementById('timer').innerHTML = duration;
+  }
 
-    if (duration == 0) {
-        timeUp();
-    }
+  if (duration == 0) {
+    timeUp();
+  }
 }
 
 function timeUp() {
-    alert("Unlucky! You ran out of time! You didn't make the cereal.... very unfortunate");
-    alert('Please click the links below to either view your stats or try again');
-    document.getElementById('failure').style.visibility = 'visible';
-    timeRem = 0;
+  alert("Unlucky! You ran out of time! You didn't make the cereal.... very unfortunate");
+  alert('Please click the links below to either view your stats or try again');
+  document.getElementById('failure').style.visibility = 'visible';
+  timeRem = 0;
 }
 
 function success() {
-    alert("CONGRATS! YOU WON, the cereal was made!!");
-    alert('Please click the links below to either view your stats or try again');
-    document.getElementById('success').style.visibility;
-    timeRem = duration;
+  alert("CONGRATS! YOU WON, the cereal was made!!");
+  alert('Please click the links below to either view your stats or try again');
+  document.getElementById('success').style.visibility;
+  timeRem = duration;
 }
 
-var character = { inventory: [], location: "bedroom" };
+var player = { inventory: [], location: "bedroom" };
 
 var area = {
-    bedroom: {
-        short_description: "bedroom",
-        contents: [],
-    },
-    kitchen: {
-        short_description: "kitchen",
-        contents: ["bowl", "cereal", "spoon", "milk"],
-    },
-    shop: {
-        short_description: "shop",
-        contents: ["cereal", "milk"],
-    }
+  bedroom: {
+    short_description: "bedroom",
+    contents: [],
+  },
+  kitchen: {
+    short_description: "kitchen",
+    contents: ["bowl", "cereal", "spoon", "milk"],
+  },
+  shop: {
+    short_description: "shop",
+    contents: ["cereal", "milk"],
+  }
 };
 
 
 function command_split(str) {
-    var parts = str.split(/\s+/); // splits string into array of words, removing whitespace
-    var command = parts.shift(); // command is the first word in the array, which is removed from the array
-    var object = parts.join(" "); // the rest of the words
-    return [command, object];
+  var parts = str.split(/\s+/); // splits string into array of words (verb + object)
+  var command = parts.shift(); // verb taken out of array
+  var object = parts.join(" "); // remaining words concatenated to create the object
+  return [command, object];
 }
 
 var room, command, verb, obj;
 
 function remove(array, item) {
-    var idx = array.indexOf(item);
-    if (idx > -1) {
-        array.splice(idx, 1);
-    }
+  var idx = array.indexOf(item);
+  if (idx > -1) {
+    array.splice(idx, 1);
+  }
 }
 
 function move_to_room(room_name) {
-    character.location = room_name;
-    room = area[room_name];
-    console.log(room);
-    if (room.short_description == "kitchen") {
-        console.log('kitchen');
-        message.innerHTML = 'You have made your way to the kitchen!';
-        document.getElementById('kitchenPic').style.display = 'block';
-    }
+  player.location = room_name;
+  room = area[room_name];
+  console.log(room);
+  if (room.short_description == "kitchen") {
+    console.log('kitchen');
+    message.innerHTML = 'You have made your way to the kitchen!';
+    document.getElementById('kitchenPic').style.display = 'block';
+  }
 }
 
 function printInventory() {
-    console.log("invcheck");
-    inventoryOpened += 1;
-    message.innerHTML = "You are carrying nothing";
-    character.inventory.forEach(function (item) {
-        message.innerHTML = "You are carrying:" + "&nbsp;&nbsp;&nbsp;&nbsp;" + item;
-    });
+  console.log("invcheck");
+  inventoryOpened += 1;
+  message.innerHTML = "You are carrying nothing";
+  player.inventory.forEach(function (item) {
+    message.innerHTML = "You are carrying:" + "&nbsp;&nbsp;&nbsp;&nbsp;" + item;
+  });
 }
 
 function take_item(obj) {
-    let found = false;
-    room.contents.forEach(function (item) {
-        if (item.includes(obj)) {
-            // does the word in obj match any part of the text of item?
-            found = true;
-            message.innerHTML = "You pick up the " + item;
-            character.inventory.push(item);
-            remove(room.contents, item);
-        }
-    });
-    if (!found) {
-        message.innerHTML = "There is no " + obj + " here.";
+  let found = false;
+  room.contents.forEach(function (item) {
+    if (item.includes(obj)) {
+      //checks if object is in the room
+      found = true;
+      message.innerHTML = "You take the " + item;
+      player.inventory.push(item);
+      remove(room.contents, item);
     }
+  });
+  if (!found) {
+    message.innerHTML = "There is no " + obj + " here.";
+  }
 }
 
-room = area[character.location];
+room = area[player.location];
 
-function getOneCommand(text) {
-    room = area[character.location];
-    command = command_split(text.toLowerCase());
-    verb = command[0];
-    obj = command[1];
-    console.log("verb: " + verb + ", object: " + obj);
-    if (["go"].includes(verb)) {
-        move_to_room(obj);
-    } else if (["inventory"].includes(verb)) {
-        printInventory();
-    } else if (["take", "pickup"].includes(verb)) {
-        //call multiple choice for that obj
-        multiChoice(obj);
-        take_item(obj);
-    } else if (["drop", "throw", "release"].includes(verb)) {
-        drop_item(obj);
-    }
+function Command(text) {
+  room = area[player.location];
+  command = command_split(text.toLowerCase());
+  verb = command[0];
+  obj = command[1];
+  console.log("verb: " + verb + ", object: " + obj);
+  if (["go"].includes(verb)) {
+    move_to_room(obj);
+  } else if (["inventory"].includes(verb)) {
+    printInventory();
+  } else if (["take", "pickup"].includes(verb)) {
+    //call multiple choice for that obj
+    multiChoice(obj);
+    take_item(obj);
+  } else if (["drop", "throw", "release"].includes(verb)) {
+    drop_item(obj);
+  }
 }
 
 //trying to get enter key working
 var input = document.getElementById("entry").value;
 input.addEventListener("keyup", function (event) {
-    if (event.keyCode === 13) {
-        event.preventDefault();
-        document.getElementById("SUBMIT").click();
-    }
+  if (event.keyCode === 13) {
+    event.preventDefault();
+    document.getElementById("SUBMIT").click();
+  }
 });
 
-function showmulti(){
-    if (room.short_description == "cereal") {
-        console.log('cereal');
-        message.innerHTML = 'Select a cereal';
-        document.getElementById('').style.display = 'block';
-    }
+function showmulti() {
+  if (room.short_description == "cereal") {
+    console.log('cereal');
+    message.innerHTML = 'Select a cereal';
+    document.getElementById('').style.display = 'block';
+  }
 }
 function textEntered() {
-    let entered = document.getElementById('entry').value;
-    let message = document.getElementById('message');
-    getOneCommand(entered);
-    inputCount += 1;
-    /*if (entered.includes('kitchen')){
-        console.log('kitchen');
-        message.innerHTML='You have made your way to the kitchen!';
-        document.getElementById('kitchenPic').style.display = 'block';
-    }*/
+  let entered = document.getElementById('entry').value;
+  let message = document.getElementById('message');
+  Command(entered);
+  inputCount += 1;
+  /*if (entered.includes('kitchen')){
+      console.log('kitchen');
+      message.innerHTML='You have made your way to the kitchen!';
+      document.getElementById('kitchenPic').style.display = 'block';
+  }*/
 }
 
 function statsPage() {
-    document.getElementById('timeRem').innerHTML = 'You finished with ' + timeRem + ' seconds left.';
-    document.getElementById('inventoryOpened').innerHTML = 'You opened your inventory ' + inventoryOpened + ' times.';
-    document.getElementById('inputCount').innerHTML = 'You entered ' + inputCount + ' commands.';
-    if (bedroom) {
-        places.push("Bedroom");
-    }
-    if (kitchen) {
-        places.push("Kitchen");
-    }
-    if (shop) {
-        places.push("Shop");
-    }
-    if (places.length == 0) {
-        places.push("... nowhere ... Try again there are many places to explore!");
-    }
-    document.getElementById('places').innerHTML = "You visited the following places: " + places;
+  document.getElementById('timeRem').innerHTML = 'You finished with ' + timeRem + ' seconds left.';
+  document.getElementById('inventoryOpened').innerHTML = 'You opened your inventory ' + inventoryOpened + ' times.';
+  document.getElementById('inputCount').innerHTML = 'You entered ' + inputCount + ' commands.';
+  if (bedroom) {
+    places.push("Bedroom");
+  }
+  if (kitchen) {
+    places.push("Kitchen");
+  }
+  if (shop) {
+    places.push("Shop");
+  }
+  if (places.length == 0) {
+    places.push("... nowhere ... Try again there are many places to explore!");
+  }
+  document.getElementById('places').innerHTML = "You visited the following places: " + places;
 }
 
 function multiChoice(obj) {
-    if (obj == "cereal") {
-        cerealMulti();
-    }
-    if (obj == "milk") {
-        milkMulti();
-    }
+  if (obj == "cereal") {
+    cerealMulti();
+  }
+  if (obj == "milk") {
+    milkMulti();
+  }
 }
 
-function cerealMulti(){
-    document.getElementById('multiChoice').style.visibility = 'visibile;'
+function cerealMulti() {
+  document.getElementById('multiChoice').style.visibility = 'visibile;'
 }
 
-function milkMulti(){
+function milkMulti() {
 
 }
